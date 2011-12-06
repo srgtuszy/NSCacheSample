19bb2354995c4554152d8db498799497c29b7787
Michał Tuszyński <srgtuszy@gmail.com>
2011-12-06 19:16:47 +0100

    Initial Commit

---
+++ NSCacheSample/APVAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  APVAppDelegate.h
+//  NSCacheSample
+//
+//  Created by Michal Tuszynski on 12/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class APVViewController;
+
+@interface APVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) APVViewController *viewController;
+
+@end
---
+++ NSCacheSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSCacheSample
+//
+//  Created by Michal Tuszynski on 12/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APVAppDelegate class]));
+    }
+}
